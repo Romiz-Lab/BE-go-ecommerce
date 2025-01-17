8bdd3c24d9f6e2ad953fdd72ee0d471d47fccf71
romiyusnandar <yusromi04@gmail.com>
2025-01-17 17:23:02 +0700

    Create connection to postgres

--- app/server.go
+++ app/server.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/lpernett/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -23,8 +24,26 @@ type AppConfig struct {
 	AppPort string
 }
 
-func (server *Server) Initialize(appConfig AppConfig) {
+type DBConfig struct {
+	DBHost string
+	DBUser string
+	DBPass string
+	DBName string
+	DBPort string
+}
+
+func (server *Server) Initialize(appConfig AppConfig, dbConfig DBConfig) {
 	fmt.Println("Welcome to "+ appConfig.AppName + " API server!")
+
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", dbConfig.DBHost, dbConfig.DBUser, dbConfig.DBPass, dbConfig.DBName, dbConfig.DBPort)
+	server.DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+    panic(err)
+  } else {
+    fmt.Println("Connected to the database!")
+  }
 
 	server.Router = mux.NewRouter()
 	server.initializeRoutes()
@@ -45,6 +64,7 @@ func getEnv(key, fallback string) string {
 func Run() {
 	var server = Server{}
 	var appConfig = AppConfig{}
+	var dbConfig = DBConfig{}
 
 	err := godotenv.Load()
 	if err != nil {
@@ -55,6 +75,12 @@ func Run() {
 	appConfig.AppEnv = getEnv("APP_ENV", "development")
 	appConfig.AppPort = getEnv("APP_PORT", "9000")
 
-	server.Initialize(appConfig)
+	dbConfig.DBHost = getEnv("DB_HOST", "localhost")
+	dbConfig.DBUser = getEnv("DB_USER", "postgres")
+	dbConfig.DBPass = getEnv("DB_PASS", "romi")
+	dbConfig.DBName = getEnv("DB_NAME", "go_commerce")
+	dbConfig.DBPort = getEnv("DB_PORT", "5432")
+
+	server.Initialize(appConfig, dbConfig)
 	server.Run(":" + appConfig.AppPort)
 }
